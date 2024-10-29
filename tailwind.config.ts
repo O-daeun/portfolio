cdb5040740ed7b1b656c426220e82927b66fcb90
daeun <qzsz797979@gmail.com>
2024-10-29 13:54:52 +0900

    init: 초기 폴더 세팅 및 초기화

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,19 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        pretendard: ['Pretendard', 'sans-serif'],
+      },
+      screens: {
+        sm: '768px', // mobile
+        md: '1024px', // tablet
       },
     },
   },
-  plugins: [],
 };
-export default config;
