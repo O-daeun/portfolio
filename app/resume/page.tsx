4aad2deac2e1d55b8f5adb538a1bba2ba4a873b0
daeun <qzsz797979@gmail.com>
2024-10-29 22:14:30 +0900

    feat: 이력서 Skill 섹션 구현

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,12 +1,14 @@
 import InnerLayout from '@/components/pages/resume-page/inner-layout';
 import IntroduceSection from '@/components/pages/resume-page/introduce-section';
 import ProfileSection from '@/components/pages/resume-page/profile-section';
+import SkillSection from '@/components/pages/resume-page/skill-section';
 
 export default function ResumePage() {
   return (
     <InnerLayout className="py-10">
       <ProfileSection />
       <IntroduceSection />
+      <SkillSection />
     </InnerLayout>
   );
 }
---
+++ components/pages/resume-page/skill-section.tsx
@@ -0,0 +1,55 @@
+import SectionTitle from './section-title';
+
+const SKILL_LIST = [
+  {
+    title: 'Languages',
+    list: ['JavaScript', 'TypeScript', 'HTML', 'CSS'],
+  },
+  {
+    title: (
+      <>
+        Frameworks & <br />
+        Libraries
+      </>
+    ),
+    list: [
+      'React.js',
+      'Next.js',
+      'React Query',
+      'Tailwind CSS',
+      'Styled Components',
+      'Axios',
+      'React Hook Form',
+      'Next Auth',
+      'Zustand',
+    ],
+  },
+  {
+    title: 'Tools',
+    list: ['Git', 'Github', 'Notion'],
+  },
+];
+
+export default function SkillSection() {
+  return (
+    <section className="mt-20">
+      <SectionTitle className="mb-10">SKILL</SectionTitle>
+      <ul className="flex flex-col gap-8">
+        {SKILL_LIST.map((skill, index) => (
+          <li key={index} className="flex gap-14 border-b border-gray-200 pb-8 last:border-none">
+            <h3 className="w-[300px] shrink-0 text-right text-3xl font-medium text-gray-500">
+              {skill.title}
+            </h3>
+            <ul className="flex w-full flex-wrap gap-y-4">
+              {skill.list.map((item, index) => (
+                <li key={index} className="w-1/2 list-disc text-lg">
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
