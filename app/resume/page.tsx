6cab62970bc49a9c19955958b209e92888bf228d
daeun <qzsz797979@gmail.com>
2024-11-05 23:35:47 +0900

    docs: 검색엔진에 검색되지 않도록 함

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -5,16 +5,22 @@ import IntroduceSection from '@/components/pages/resume-page/introduce-section';
 import ProfileSection from '@/components/pages/resume-page/profile-section';
 import ProjectSection from '@/components/pages/resume-page/project-section';
 import SkillSection from '@/components/pages/resume-page/skill-section';
+import Head from 'next/head';
 
 export default function ResumePage() {
   return (
-    <InnerLayout className="py-10">
-      <ProfileSection />
-      <IntroduceSection />
-      <SkillSection />
-      <ExperienceSection />
-      <ProjectSection />
-      <EducationSection />
-    </InnerLayout>
+    <>
+      <Head>
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
+      <InnerLayout className="py-10">
+        <ProfileSection />
+        <IntroduceSection />
+        <SkillSection />
+        <ExperienceSection />
+        <ProjectSection />
+        <EducationSection />
+      </InnerLayout>
+    </>
   );
 }
